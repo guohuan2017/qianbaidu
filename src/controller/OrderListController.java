0630de5e130dce54a555d08f92b07d66f8da1bac
Administrator <Administrator@192.168.1.125>
2017-07-20 15:11:11 +0800

    cashdesk收银页面

--- src/controller/OrderListController.java
+++ src/controller/OrderListController.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import pojo.OrderList;
 import service.OrderListService;
@@ -11,4 +13,9 @@ public class OrderListController {
 
 	@Autowired
 	private OrderListService service;
+	
+	@RequestMapping("/pay.action")
+	public ModelAndView zhifu(){
+		return new ModelAndView("test/tmp","message","支付成功");
+	}
 }
