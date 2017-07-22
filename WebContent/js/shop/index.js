88db44ec033167a7b875d43cfb7af6941192ebcd
gyx <776295498@qq.com>
2017-07-22 20:23:16 +0800

    调整导航栏出现的高度

--- WebContent/js/shop/index.js
+++ WebContent/js/shop/index.js
@@ -1,6 +1,6 @@
 window.onscroll = function(){
 	console.log(document.body.scrollTop);
-	if(document.body.scrollTop >= 392){
+	if(document.body.scrollTop >= 370){
 		$("#nav-left").show();
 	}else{
 		$("#nav-left").hide();
